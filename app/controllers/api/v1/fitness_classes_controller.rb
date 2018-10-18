3002e7c3558e0bac57ef38e3ca39893ef4cdb56c
ELehotzky <Lehotzky.Erica@gmail.com>
2018-10-18 17:38:41 -0500

    updates to controller

--- app/controllers/api/v1/fitness_classes_controller.rb
+++ app/controllers/api/v1/fitness_classes_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::FitnessClassesController < ApplicationController
 		render json: @fitness_classes 
 	end
 
+	def create
+		@fitness_class = FitnessClass.create(fitness_class_params)
+		if @fitness_class.valid?
+			render json: {fitness_class: AdminSerializer.new(@fitness_class), jwt: @token}, status: :created
+		else
+			render json: {error: "Please try again"}, status: :not_acceptable
+		end
+	end
+
+
+	private 
+
+	def fitness_class_params
+		params.permit(:name, :description, :image, :class_length, :price)
+	end
+
 end
