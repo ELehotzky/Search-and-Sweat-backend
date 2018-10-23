118b3b5c5fd1a79cd418d98c84c06e78cc20353e
ELehotzky <Lehotzky.Erica@gmail.com>
2018-10-23 15:16:31 -0500

    fixed forms with Josh/Humzah help

--- app/controllers/api/v1/fitness_classes_controller.rb
+++ app/controllers/api/v1/fitness_classes_controller.rb
@@ -10,10 +10,10 @@ class Api::V1::FitnessClassesController < ApplicationController
 		cd = ClassDetail.create(time: params[:time], instructor: "tbd")
 		fcd = FitnessClassDetail.create(fitness_class_id: @fitness_class.id, class_detail_id: cd.id)
 
-		fc = format_fitness_class(@fitness_class, cd, params[:categories])
+		# fc = format_fitness_class(@fitness_class, cd, params[:categories])
 		if @fitness_class.valid?
 			 # render json: {fitness_class: FitnessClassSerializer.new(@fitness_class)}, status: :created
-			render json: {fitness_class: fc}, status: :created
+			render json: @fitness_class, include:[ :fitness_class_categories, :fitness_class_details], status: :created
 			# render json: {fitness_class: @fitness_class, jwt: @token}, status: :created
 		else
 			render json: {error: "Please try again"}, status: :not_acceptable
@@ -24,7 +24,7 @@ class Api::V1::FitnessClassesController < ApplicationController
 	private 
 
 	def fitness_class_params
-		params.permit(:name, :studio_id, :description, :image, :class_length, :price)
+		params.permit(:name, :studio_id, :description, :image, :class_length, :price, category_ids:[])
 	end
 
 	def format_fitness_class(fc, cd, categories)
